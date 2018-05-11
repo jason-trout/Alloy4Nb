fdcdab603176b5bff79f174adab8ec8080a33aa0
Jason E. Trout <jsnetrout@gmail.com>
2018-05-11 08:48:37 -0500

    Initial commit

---
+++ build/classes-generated/jet3238/alloy4nb/Bundle.java
@@ -0,0 +1,27 @@
+package jet3238.alloy4nb;
+/** Localizable strings for {@link jet3238.alloy4nb}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Source</i>
+     * @see AlloyDataObject
+     */
+    static String LBL_Alloy_EDITOR() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_Alloy_EDITOR");
+    }
+    /**
+     * @return <i>Files of Alloy</i>
+     * @see AlloyDataObject
+     */
+    static String LBL_Alloy_LOADER() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_Alloy_LOADER");
+    }
+    /**
+     * @return <i>Visual</i>
+     * @see AlloyVisualElement
+     */
+    static String LBL_Alloy_VISUAL() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "LBL_Alloy_VISUAL");
+    }
+    private Bundle() {}
+}
